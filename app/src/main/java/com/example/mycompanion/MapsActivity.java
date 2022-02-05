4d76d6852a59ba2a1739fd6970635ab3c2c7a2ec
Ruchi-23 <ruchira2306@gmail.com>
2022-02-06 02:45:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mycompanion/MapsActivity.java
@@ -0,0 +1,103 @@
+package com.example.mycompanion;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import static java.lang.String.valueOf;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private  final long MIN_TIME =1000;
+    private final long MIN_DIST = 5;
+    private LatLng latLng;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.SEND_SMS}, PackageManager.PERMISSION_GRANTED);
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED);
+
+
+    }
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                try{
+                    DBHelper db = new DBHelper(MapsActivity.this);
+                    latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title("Your Location"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                    String message = "http://maps.google.com/?q=" + location.getLatitude() + "," + location.getLongitude();
+
+
+                    Cursor cursor=new DBHelper(getApplicationContext()).readalldata();
+
+                    String phoneNumber = null;
+
+                    while(cursor.moveToNext())
+                    {
+                        phoneNumber =cursor.getString(3);
+
+                    }
+
+                    SmsManager smsManager = SmsManager.getDefault();
+                    StringBuffer smsBody = new StringBuffer();
+
+                    smsManager.sendTextMessage(phoneNumber, null, message, null, null);
+
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        };
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        try {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME,MIN_DIST,locationListener);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME,MIN_DIST,locationListener);
+        }
+        catch (SecurityException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
